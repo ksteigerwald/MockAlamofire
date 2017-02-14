c65ce644dcf5401c04a22adbf9eab21cae9d90cf
Kris Steigerwald <kris.steigerwald@capitalone.com>
2017-02-13 22:14:47 -0500

    Initial Commit

---
+++ MockAlamofire/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MockAlamofire
+//
+//  Created by Steigerwald, Kris S. (CONT) on 2/13/17.
+//  Copyright © 2017 Velaru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
