1a9a01379ed4935974e224d16f293cbec4461ff9
Kris Steigerwald <kris.steigerwald@capitalone.com>
2017-02-13 22:16:50 -0500

    - update pod, install

---
+++ Pods/Target Support Files/Pods-MockAlamofire/Pods-MockAlamofire-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MockAlamofire : NSObject
+@end
+@implementation PodsDummy_Pods_MockAlamofire
+@end
---
+++ Pods/Target Support Files/Pods-MockAlamofire/Pods-MockAlamofire-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_MockAlamofireVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_MockAlamofireVersionString[];
+
