cfed795e14b67b289005824f26596d6e429dad00
Owen Thomas <owen.h.thomas@gmail.com>
2017-03-06 19:18:47 -0800

    Main file updates
    Removes force unwraps
    Converts enum cases to lowercase (Swift 3 convention)
    Removes some whitespace
    Removes many unnecessary type declarations (i.e. let token: String = “string”)
    Converts some optional properties to non-optional where it’s unnecessary
    Removes some stored properties that are only used in one method
    Makes Mocks methods/properties static since they we’re allways called on a blank instance anyway
    Removes global requestor property entirely
    Converts MockDirection into a string-backed enum and removed bulky initializer
    Converts many methods and properties to private
    Updates XCode recommended settings changes (CLANG_WARN_SUSPICIOUS_MOVE = YES)
    Possibly some other misc. changes.

--- MockAlamofire/MockingProtocol.swift
+++ MockAlamofire/MockingProtocol.swift
@@ -10,14 +10,10 @@ import Foundation
 import Alamofire
 
 class MockingURLProtocol: URLProtocol {
-    
-    let mocks:Mocks = Mocks()
-    
-    var cannedResponse: NSData?
-    let cannedHeaders = ["Content-Type" : "application/json; charset=utf-8"]
+    private let cannedHeaders = ["Content-Type" : "application/json; charset=utf-8"]
     
     // MARK: Properties
-    struct PropertyKeys {
+    private struct PropertyKeys {
         static let handledByForwarderURLProtocol = "HandledByProxyURLProtocol"
     }
     
@@ -25,36 +21,24 @@ class MockingURLProtocol: URLProtocol {
         let configuration: URLSessionConfiguration = {
             let configuration = URLSessionConfiguration.ephemeral
             configuration.httpAdditionalHeaders = SessionManager.defaultHTTPHeaders
-            
             return configuration
         }()
-        
         let session = Foundation.URLSession(configuration: configuration, delegate: self, delegateQueue: nil)
-        
         return session
     }()
     
-    var activeTask: URLSessionTask?
-    
     // MARK: Class Request Methods
     override class func canInit(with request: URLRequest) -> Bool {
-        if URLProtocol.property(forKey: PropertyKeys.handledByForwarderURLProtocol, in: request) != nil {
-            return false
-        }
-        
-        return true
+        return URLProtocol.property(forKey: PropertyKeys.handledByForwarderURLProtocol, in: request) == nil
     }
     
     override class func canonicalRequest(for request: URLRequest) -> URLRequest {
-        if let headers = request.allHTTPHeaderFields {
-            do {
-                return try URLEncoding.default.encode(request, with: headers)
-            } catch {
-                return request
-            }
+        guard let headers = request.allHTTPHeaderFields else { return request }
+        do {
+            return try URLEncoding.default.encode(request, with: headers)
+        } catch {
+            return request
         }
-        
-        return request
     }
     
     override class func requestIsCacheEquivalent(_ a: URLRequest, to b: URLRequest) -> Bool {
@@ -63,26 +47,21 @@ class MockingURLProtocol: URLProtocol {
     
     // MARK: Loading Methods
     override func startLoading() {
-        let data:NSData = mocks.find(request) as! NSData
-        let client = self.client
-        let response = HTTPURLResponse(url: (request.url)!, statusCode: 200, httpVersion: "HTTP/1.1", headerFields: cannedHeaders)
-        
-        client?.urlProtocol(self, didReceive: response!, cacheStoragePolicy: URLCache.StoragePolicy.notAllowed)
-        cannedResponse = data
-        client?.urlProtocol(self, didLoad: cannedResponse as! Data)
+        if let data = Mocks.find(request),
+            let url = request.url,
+            let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: "HTTP/1.1", headerFields: cannedHeaders) {
+            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: URLCache.StoragePolicy.notAllowed)
+            client?.urlProtocol(self, didLoad: data)
+        }
         client?.urlProtocolDidFinishLoading(self)
-        
-        activeTask?.resume()
     }
     
     override func stopLoading() {
-        activeTask?.cancel()
     }
 }
 
+// MARK: NSURLSessionDelegate extension
 extension MockingURLProtocol: URLSessionDelegate {
-    
-    // MARK: NSURLSessionDelegate
     func URLSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceiveData data: Data) {
         client?.urlProtocol(self, didLoad: data)
     }
@@ -91,7 +70,6 @@ extension MockingURLProtocol: URLSessionDelegate {
         if let response = task.response {
             client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
         }
-        
         client?.urlProtocolDidFinishLoading(self)
     }
 }
